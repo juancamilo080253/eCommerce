c7c080df2ac9e7048a730d91d2889313102b321e
Camilo Gomez <juancamilo080253@gmail.com>
2022-10-20 23:50:49 -0500

    Base de proyecto

---
+++ proyecto_back/config/database.js
@@ -0,0 +1,15 @@
+// colocamos todo el proceso de conexion con la bd 
+
+const mongoose = require("mongoose");
+const host = "localhost";
+const port = "27017";
+const db = "hr";
+
+
+exports.mongoConnect = () => {
+    const mongoStringConnection = `mongodb://${host}:${port}/${db}`;
+    mongoose.connect(mongoStringConnection);
+    mongoose.Promise = global.Promise;
+    const dbConnection = mongoose.connection;
+    dbConnection.on("error", console.error.bind(console,"Mongodb connection error"))
+}
