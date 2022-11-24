45d6d4d55cf17f9bbc29233248b36e91723202e3
Camilo Gomez <juancamilo080253@gmail.com>
2022-11-23 23:59:46 -0500

    Actualizacion de proyecto hasta el 10/11

--- ecommerce_front/src/components/login/login.js
+++ ecommerce_front/src/components/login/login.js
@@ -1,79 +1,86 @@
 import React from "react";
 import axios from "axios";
-import { Container, Form, Button, Row, Col } from "react-bootstrap";
+import { Container, Form, Button, Row, Col  } from "react-bootstrap";
 import app from "../../app.json";
 import './login.css';
-import { isNull } from "util";
+import {isNull} from "util";
 import Cookies from "universal-cookie";
-import { calcularExpirarSesion } from "../helper/helper";
+import { calcularExpirarSesion } from "../helper/helper"; //funcion se usan corchetes
+import Loading from "../loading/loading";// la clase va en mayuscula inicial
 
-const {APIHOST}=app;
+const {APIHOST} = app;
 const cookies = new Cookies();
 
 export default class login extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      Loading: false,
       usuario: "",
       pass: "",
     };
   }
 
-  iniciarSesion() {
-    axios.post(`${APIHOST}/usuarios/login`,{
-      usuario: this.state.usuario,
-      pass: this.state.pass,
-    })
-    .then((response) => {
-      if(isNull(response.data.token)){
-        alert("Usuario y/o contraseña invalidos")
-      }
-      else{
-        cookies.set('_s',response.data.token,
-        {
-          path:'/',
-          expires:calcularExpirarSesion(),
-        });
-      }
-    })
-    .catch((err)=> {
-      console.log(err);
-    })
-    alert("Boton de inicio de sesión");
-  }
-
+iniciarSesion(){ 
+  this.setState({Loading:true});
+  axios.post(`${APIHOST}/usuarios/login`,{
+    usuario: this.state.usuario,
+    pass:this.state.pass,
+  })
+  .then((response) => {
+    if(isNull(response.data.token)){
+      alert("Usuario y/o contraseña invalidos");
+    }
+    else {
+      cookies.set('_s' , response.data.token,
+      {
+        path: "/",
+        expires: calcularExpirarSesion(),
+      });
+      this.props.history.push(window.open('/empleados'));
+    }
+    this.setState({Loading:false});
+  })
+  .catch((err) =>{
+    console.log(err);
+    this.setState({Loading:false});
+  })
+  //  alert('Boton de iniciar sesion');
+}
   render() {
     return (
-      <Container id="login-container">
+      <Container id="login-container" >
+        <Loading show={this.state.Loading}/>
         <Row>
           <Col
           sm="12"
           xs="12"
-          md={{span: 4 , offset:4 }}
-          lg={{span: 4 , offset:4 }}
-          xl={{span: 4 , offset:4 }}>
+          md={{span: 4, offset:4}}
+          lg={{span: 4, offset:4}}
+          xl={{span: 4, offset:4}}>
             <Row>
-              <h2> Iniciar sesión </h2>
+              <h2>Iniciar sesión</h2>
             </Row>
             <Row>
               <Form>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
-                  <Form.Label>Usuario</Form.Label>
+                  <Form.Label id="form-label" >Usuario</Form.Label>
                   <Form.Control
                     onChange={(e) => this.setState({ usuario: e.target.value })}
                   />
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
-                  <Form.Label>Contraseña</Form.Label>
+                  <Form.Label id="form-label">Contraseña</Form.Label>
                   <Form.Control
                     type="password"
                     onChange={(e) => this.setState({ pass: e.target.value })}
                   />
                 </Form.Group>
+
                 <Button
                   variant="primary"
-                  onClick={() => {
+                  onClick={()=>{
                     this.iniciarSesion();
                   }}
                 >
@@ -83,7 +90,7 @@ export default class login extends React.Component {
             </Row>
           </Col>
         </Row>
-      </Container>
+      </Container> 
     );
   }
 }
--- ecommerce_front/src/components/router/router.js
+++ ecommerce_front/src/components/router/router.js
@@ -1,22 +1,26 @@
 //direccionamiento
-import React from 'react';
-import {BrowserRouter as Router,Route,Routes} from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Login from "../login/login";
 import Inicio from "../index/index";
 
-export default function Approutes (){
-    return(
-        <Router>
-            <Routes>
-                <Route exact path = "/login" element={<Login />}></Route>
-                <Route exact path = "/*" element={(
-                    <h1 style={{marginTop:300}}>
-                        404 <br/> pagina no encontrada
-                    </h1>
-                )}></Route> 
-                <Route exact path = "/index" element={<Inicio />}></Route>
-                <Route exact path = "/" element={<Inicio />}></Route>
-            </Routes>
-        </Router>
-    );
+export default function Approutes() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path={["/login"]} component={Login} />
+        <Route exact path={["/"]} component={Inicio} />
+        <Route
+          path={"*"}
+          component={() => (
+            <h1 style={{ marginTop: 300 }}>
+              404
+              <br />
+              Pagina no encontrada
+            </h1>
+          )}
+        />
+      </Switch>
+    </Router>
+  );
 }
