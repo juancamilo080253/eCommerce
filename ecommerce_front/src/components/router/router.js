1d66d8d88dc9e86bb823c138ef39111082011ce1
Camilo Gomez <juancamilo080253@gmail.com>
2022-11-24 00:53:54 -0500

    Creacion de grid, barra de busquedas actualizacion hasta el 17/11

--- ecommerce_front/src/components/router/router.js
+++ ecommerce_front/src/components/router/router.js
@@ -1,25 +1,25 @@
-//direccionamiento
+//direcionamiento
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Login from "../login/login";
 import Inicio from "../index/index";
+import PrivateRoute from "../auth/privaterouter";
+import empleados from "../empleados/empleados.buscar";
 
-export default function Approutes() {
+export default function appRouter() {
   return (
     <Router>
       <Switch>
+        <PrivateRoute exact path={["/empleados"]} component={empleados} />
         <Route exact path={["/login"]} component={Login} />
         <Route exact path={["/"]} component={Inicio} />
-        <Route
-          path={"*"}
-          component={() => (
-            <h1 style={{ marginTop: 300 }}>
-              404
-              <br />
-              Pagina no encontrada
+        <Route path={"*"} component={() =>(
+            <h1 style={{ marginTop:300}}>
+            404
+            <br/>
+            Pagina no encontrada
             </h1>
-          )}
-        />
+          )}/> 
       </Switch>
     </Router>
   );
